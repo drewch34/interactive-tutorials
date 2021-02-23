afe5aab72fb9109b16979852c9fa5c45de5f5b5c
Drew <firefan34@gmail.com>
2021-02-22 23:39:17 -0500

    outline for code

--- ifelse2.0/script.js
+++ ifelse2.0/script.js
@@ -1,2 +1,33 @@
 // script.js
 
+let weatherConditions1 = [sunny, rainy, snowy];
+
+//outifts1 = sunglasses, jacket, umbrella
+
+//if rainy and no umbrella = wet and sad
+//if sunny and no sunglasses = hands trying to block sun and sad
+//if snowy and no jacket = shivering
+
+let weatherConditions2a = [sunny, rainy, snowy];
+
+let weatherConditions2b = [foggy, cold, humid, hot, smoggy, windy];
+
+//sunny = [1, 2, 3, 4, 5], rainy = [0,1,2,3,4,5], snowy = [0,1,4,5]
+
+//outfits2 = sunglasses, jacket, umbrella, mask, flashlight, fan, hat, boots
+
+//only hold two things (umbrella, flashlight, fan)
+
+//if smoggy and no mask = coughing
+//if foggy and no flashlight = hand up to head, searching
+//if humid and no fan = tongue out, panting
+//if cold and no jacket = shivering
+//if hot and no fan = tongue out, panting
+//if windy and not jacket = shivering
+
+//if windy and hat/fan/mask = goodbye item
+//if snowy and cold and no jacket + hat = blue area
+//if foggy and sunglasses = bump into screen, hands in front, blind
+
+
+//easter egg = sunny/hot = beach day
