8ce2c4be30f0a72cd6cab12fca473924da6bbaf7
Drew <firefan34@gmail.com>
2021-02-28 16:18:33 -0500

    random weather function

--- ifelse2.0/script.js
+++ ifelse2.0/script.js
@@ -1,6 +1,4 @@
-// script.js
-
-let weatherConditions1 = [sunny, rainy, snowy];
+//***WEATHER CONDITIONS OPTIONS 1***
 
 //outifts1 = sunglasses, jacket, umbrella
 
@@ -8,9 +6,73 @@ let weatherConditions1 = [sunny, rainy, snowy];
 //if sunny and no sunglasses = hands trying to block sun and sad
 //if snowy and no jacket = shivering
 
-let weatherConditions2a = [sunny, rainy, snowy];
 
-let weatherConditions2b = [foggy, cold, humid, hot, smoggy, windy];
+var conditionsObject = {
+  "Sunny": {
+    "Sunglasses": ["Jacket", "Umbrella"],
+    "Jacket": ["Sunglasses", "Umbrella"],
+    "Umbrella": ["Sunglasses", "Jacket"]
+  },
+  "Rainy": {
+    "Sunglasses": ["Jacket", "Umbrella"],
+    "Jacket": ["Sunglasses", "Umbrella"],
+    "Umbrella": ["Sunglasses", "Jacket"]
+  },
+  "Snowy": {
+    "Sunglasses": ["Jacket", "Umbrella"],
+    "Jacket": ["Sunglasses", "Umbrella"],
+    "Umbrella": ["Sunglasses", "Jacket"]
+  }
+};
+
+
+var weatherConditions1 = ["sunny", "rainy", "snowy"];
+
+window.onload = function() {
+  var weatherSel = document.getElementById("weather1");
+  var outfitSel = document.getElementById("outfit1");
+  var elseOutfitSel = document.getElementById("else1");
+  var submitBtn = document.getElementById("submit");
+
+  for (var x in conditionsObject) {
+    weatherSel.options[weatherSel.options.length] = new Option(x, x);
+  }
+  weatherSel.onchange = function() {
+    //empty Chapters- and Topics- dropdowns
+    outfitSel.length = 1;
+    elseOutfitSel.length = 1;
+    //display correct values
+    for (var y in conditionsObject[this.value]) {
+      outfitSel.options[outfitSel.options.length] = new Option(y, y);
+    }
+  }
+  outfitSel.onchange = function() {
+    //empty Chapters dropdown
+    elseOutfitSel.length = 1;
+    //display correct values
+    var z = conditionsObject[weatherSel.value][this.value];
+    for (var i = 0; i < z.length; i++) {
+      elseOutfitSel.options[elseOutfitSel.options.length] = new Option(z[i], z[i]);
+    }
+  }
+
+  submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var randomWeatherSelect = weatherConditions1[Math.floor(Math.random() * weatherConditions1.length)];
+    document.getElementById("randomWeather").innerHTML = randomWeatherSelect;
+  });
+
+};
+
+
+
+
+
+
+
+// let weatherConditions2a = [sunny, rainy, snowy];
+
+// let weatherConditions2b = [foggy, cold, hot, smoggy, windy];
 
 //sunny = [1, 2, 3, 4, 5], rainy = [0,1,2,3,4,5], snowy = [0,1,4,5]
 
